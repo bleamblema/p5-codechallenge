4e3fbb8d085ea5f569083c760e25babea504221e
bleam <bleamblema@gmail.com>
2016-05-06 01:00:27 +0700

    complete

--- starfield/starfield/sketch.js
+++ starfield/starfield/sketch.js
@@ -2,7 +2,7 @@ var stars = [];
 
 function setup() {
   createCanvas(600, 400);
-  for(var i = 0; i < 500; i++){
+  for(var i = 0; i < 800; i++){
     stars[i] = new Star();
   }
 }
@@ -20,14 +20,16 @@ function Star() {
   this.x = random(-width, width);
   this.y = random(-height, height);
   this.z = random(width);
+  this.fz = this.z;
+  
   
   this.update = function() {
-    this.z -= 10;
+    this.z -= 20;
     if(this.z < 1){
       this.z = width;
       this.x = random(-width, width);
       this.y = random(-height, height);
-      
+      //this.fx = this.z;
     }
     
   }
@@ -39,6 +41,14 @@ function Star() {
     var sy = map(this.y / this.z, 0, 1, 0, height);
     var r = map(this.z, 0, width, 16, 0);
     
-    ellipse(sx, sy, r, r);
+    //ellipse(sx, sy, r, r);
+    
+    var fx =map(this.x / this.fz, 0, 1, 0, width);
+    var fy =map(this.y / this.fz, 0, 1, 0, height);
+    this.fz = this.z;
+    
+    stroke(255);
+    if(fx-sx > 0 && fx < 0 || fx-sx < 0 && fx > 0)
+      line(fx, fy, sx, sy);
   }
 }
