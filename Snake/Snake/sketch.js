8c91375d6c5c51a936e8d6c53ae2c9c758f87989
bleam <bleamblema@gmail.com>
2016-05-06 12:57:48 +0700

    snake complete

--- Snake/Snake/sketch.js
+++ Snake/Snake/sketch.js
@@ -1,19 +1,41 @@
 var s;
 var scl = 20;
+var food;
 
 function setup() {
+  frameRate(15);
   var canv = createCanvas(600, 600);
   canv.position(windowWidth/2 - width/2, windowHeight/2 - height/2);
   s = new Snake();
+  pickLocation();
   var h1 = createElement('h1', "The Snake Game");
   h1.style("margin", "0 auto");
   h1.style("text-align", "center");
 }
 
+function pickLocation(){
+  var cols = floor(width/scl);
+  var rows = floor(height/scl);
+  food = createVector(floor(random(cols)), floor(random(rows)));
+  food.mult(scl);
+  
+}
+
+function mousePressed(){
+  s.total++;
+}
+
 function draw() {
   background(51);
+  if(s.eat(food)){
+    pickLocation();
+  }
+  s.death();
   s.update();
   s.show();
+  
+  fill(255, 0, 100);
+  rect(food.x, food.y, scl, scl)
 }
 
 function keyPressed(){
@@ -33,18 +55,57 @@ function Snake(){
   this.y = 0;
   this.xspeed = 1;
   this.yspeed = 0;
+  this.total = 0;
+  this.tail = [];
+  
   this.update = function() {
+    if(this.total == this.tail.length){ //when it have tails
+      for(var i=0; i< this.tail.length; i++)
+        this.tail[i] = this.tail[i+1];
+    }
+    if(this.total >= 0)
+      this.tail[this.total-1] = createVector(this.x, this.y);
+    
     this.x = this.x + this.xspeed*scl;
     this.y = this.y + this.yspeed*scl;
-    this.x = constrain(this.x, 0, width-scl);
-    this.y = constrain(this.y, 0, height-scl);
+  
+    if(this.x > width-scl)
+      this.x = 0;
+    else if(this.x < 0)
+      this.x = width-scl;
+    if(this.y > height-scl)
+      this.y = 0;
+    else if(this.y < 0)
+      this.y = height-scl;
   }
+
   this.show = function() {
     fill(255);
-    rect(this.x, this.y, scl, scl)
+    
+    for(var i=0; i < this.tail.length; i++)
+      rect(this.tail[i].x, this.tail[i].y, scl, scl);
+    rect(this.x, this.y, scl, scl);
   }
   this.dir = function(x, y) {
     this.xspeed = x;
     this.yspeed = y;
+  }
+  this.eat = function(pos){
+    var d = dist(this.x, this.y, pos.x, pos.y);
+    if(d < 1){
+      this.total++;
+      return true;
+    }else
+      return false;
+  }
+  this.death = function() {
+    for(var i = 0; i < this.tail.length; i++){
+      var pos = this.tail[i];
+      var d = dist(this.x, this.y, pos.x, pos.y);
+      if(d < 1){
+        this.total = 0;
+        this.tail = [];
+      }
+    }
   }
 } 
